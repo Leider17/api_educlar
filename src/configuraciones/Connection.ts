19bc5ace915f58dbe19e35db4e7e5db7bfeb7968
Leider17 <leidersolano133@gmail.com>
2024-12-06 12:08:44 -0500

    conexion a postgresql

--- src/configuraciones/Connection.ts
+++ src/configuraciones/Connection.ts
@@ -1,1 +1,29 @@
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export const myDataSource = new DataSource({
+    type: "postgres", 
+    host: process.env.DB_HOST,
+    port: Number(process.env.PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: ["src/entidades/*.ts"],
+    logging: process.env.DB_LOGGING === "true",
+    synchronize: process.env.DB_SYNCHRONIZE === "true",
+});
+
+export const connectToDatabase = async () => {
+    try {
+        await myDataSource.initialize();
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Error connecting to the database", error);
+        process.exit(1); 
+    }
+};
+
+
 
--- src/configuraciones/Server.ts
+++ src/configuraciones/Server.ts
@@ -2,35 +2,40 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import express from "express";
-//import Connection from "./Connection";
+import { connectToDatabase } from "./Connection";
 
 class Server {
-    public app:express.Application;
+    public app: express.Application;
 
     constructor() {
-        dotenv.config({ path:"variable.env" });
-       // Connection();
+        dotenv.config({ path: ".env" });
         this.app = express();
         this.startConf();
         this.startRoutes();
-    };
+    }
 
     public startConf() {
         this.app.set("PORT", process.env.PORT);
         this.app.use(cors());
         this.app.use(morgan("dev"));
-        this.app.use(express.json({ limit:"50MB" }));
-        this.app.use(express.urlencoded({ extended:true }));
-    };
+        this.app.use(express.json({ limit: "50MB" }));
+        this.app.use(express.urlencoded({ extended: true }));
+    }
 
     public startRoutes() {
-    };
+        // Define tus rutas aquí
+    }
 
-    public startServer() {
-        this.app.listen(this.app.get("PORT"), ()=>{
-            console.log("Backend ready in the port:", this.app.get("PORT"));
-        });
-    };
+    public async startServer() {
+        try {
+            await connectToDatabase(); 
+            this.app.listen(this.app.get("PORT"), () => {
+                console.log("Backend ready on port:", this.app.get("PORT"));
+            });
+        } catch (error) {
+            console.error("Failed to start the server:", error);
+        }
+    }
 }
 
-export default Server;
+export default Server;
